5d6a77dce571adbb963e7c8b3af562e1df453886
hmiao <739025250@qq.com>
2023-05-29 15:26:15 +0800

    fix spelling error

--- web/src/components/navbar.vue
+++ web/src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header">
     <router-link :to="{ name: 'playground' }">
-      <span class="logo">Cyrnux Developer Console</span>
+      <span class="logo">Crynux Developer Console</span>
     </router-link>
     <div class="right-menu">
       <el-menu
