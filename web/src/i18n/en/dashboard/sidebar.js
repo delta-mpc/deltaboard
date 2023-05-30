45a0be6952bdc9bffa8dd7597efd5830a3b2c02b
hmiao <739025250@qq.com>
2023-05-30 09:47:17 +0800

    change button name

--- web/src/i18n/en/dashboard/sidebar.js
+++ web/src/i18n/en/dashboard/sidebar.js
@@ -1,7 +1,7 @@
 export default {
    'dashboard.sidebar.playground':'Playground',
    'dashboard.sidebar.tasklist':'Tasks',
-   'dashboard.sidebar.nodelist':'Cluster Nodes',
+   'dashboard.sidebar.nodelist':'Network',
    'dashboard.sidebar.user_management' : 'User Management',
-   'dashboard.sidebar.personal_profile' : 'Personal Profile'
+   'dashboard.sidebar.personal_profile' : 'Profile'
 }
